d039a5f5609f505b05e4de52a110c8b7262f99f6
Lassi Chen <lachen@utu.fi>
2024-07-20 03:25:51 +0300

    entries are now stored in browser localStorage

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 const button = document.getElementById("new_entry")
 const textbox = document.getElementById("entry_textbox")
+const htmlZone = document.getElementById("entries")
+
+const thelist = JSON.parse(localStorage.getItem("data")) || []
+
 
 const createEntry = () => {
-    const htmlZone = document.getElementById("entries")
-    const entry = `<p><input type="checkbox">${textbox.value}</p>`
-    htmlZone.insertAdjacentHTML("beforeend", entry)
+    const entryData = {
+        state: false,
+        title: textbox.value,
+        content: "",
+        creation_time: new Date().toISOString(),
+        completion_time: null,
+    }
+
+    thelist.push(entryData)
+    localStorage.setItem("data", JSON.stringify(thelist));
+    updateView()
 
     textbox.value = ""
 }
+
+const updateView = () => {
+    htmlZone.innerHTML = ""
+
+    thelist.forEach((element) => {
+        htmlZone.innerHTML += `<p><input type="checkbox">${element.title}</p>`
+    })
+}
+
+
+updateView()
 
 button.addEventListener("click", createEntry)
